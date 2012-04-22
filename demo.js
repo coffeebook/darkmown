2817a9207db0d0561109bae926fcba03bde433cd
Denis Ciccale <dciccale@gmail.com>
2012-04-23 00:34:03 +0200

    demo page

---
+++ demo.js
@@ -0,0 +1,22 @@
+(function() {
+
+  $(function() {
+    var $htmlResult, $renderedResult, $userInput, converter, showResult;
+    converter = new Darkmown.converter();
+    $userInput = $('#user_input');
+    $renderedResult = $('#result');
+    $htmlResult = $('#html_result');
+    showResult = function(html) {
+      var value;
+      value = $userInput.val();
+      html = converter.makeHtml(value);
+      $renderedResult.html(html);
+      return $htmlResult.val(html.replace(/>/g, ">\n").replace(/</g, "\n<").replace(/\n{2,}/g, "\n\n"));
+    };
+    $userInput.keyup(function() {
+      return showResult();
+    });
+    return showResult();
+  });
+
+}).call(this);
